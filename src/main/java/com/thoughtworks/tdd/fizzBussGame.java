2c52a093d612f999671a180534b05ae06593ccc9
fanyafeng9831 <1090986665@qq.com>
2019-08-23 15:46:26 +0800

    game 3 and 1

---
+++ src/main/java/com/thoughtworks/tdd/fizzBussGame.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.tdd;
+
+public class fizzBussGame {
+
+	public String fizzBuss(int number) {
+		// TODO Auto-generated method stub
+		if(number%3==0) {
+			return "fizz";
+		}
+		return "1";
+	}
+	
+
+}
---
+++ src/test/java/com/thoughtworks/tdd/fizzBussGameTest.java
@@ -0,0 +1,29 @@
+package com.thoughtworks.tdd;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class fizzBussGameTest {
+	@Test
+	public void fizzBuss_given_1_then_1() {
+		//given
+		int number=1;
+		fizzBussGame fizzBussGameNew =new fizzBussGame();
+		
+		//when
+		String word = fizzBussGameNew.fizzBuss(number);
+		//then
+		Assertions.assertEquals("1",word);
+	}
+	@Test
+	public void fizzBuss_given_3_then_fizz() {
+		//given
+		int number=3;
+		fizzBussGame fizzBussGameNew =new fizzBussGame();
+		
+		//when
+		String word = fizzBussGameNew.fizzBuss(number);
+		//then
+		Assertions.assertEquals("fizz",word);
+	}
+
+}
